78f5ca381237916c4cb01f3f3da8d9df1c47d078
Vishnu Prasad Gettam <60579222+VishnuGettam@users.noreply.github.com>
2023-01-30 21:26:54 +0000

    Update Datasampling.sql

--- Datasampling.sql
+++ Datasampling.sql
@@ -1,1 +1,27 @@
 
+--data sampling with ROW/BERNOLI method or SYSTEM/BLOCK method along with uses FRACTION or FIXED-SIZE ways
+
+--FRACTION METHOD
+
+--ROW method(will get the (p/100) * table count of records)
+--(12/100) * count of table rows
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample row(12);
+select * from snowflake_sample_data.tpcds_sf100tcl.customer tablesample row(12);
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample bernoulli(8);
+
+--SYSTEM method(will fetch data (p/100) * number of micro-partitions)
+--will get (7/100) * number of micro-partitions 
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample system(7);
+select * from snowflake_sample_data.tpcds_sf100tcl.customer tablesample block(7);
+
+--FIXED METHOD (Supported by ROW method only)
+
+--will fetch records with fixed size of 10 rows
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample(10 rows);
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample row(10 rows);
+
+--SEED or REPEATABLE 
+--to get the same deterministic data for multiple executions we used SEED or REPEATABLE
+--we have to assing a number to the SEED(number) which will register to get the same set of data for all executions
+select * from snowflake_sample_data.tpcds_sf100tcl.customer sample row(12) SEED(125);
+select * from snowflake_sample_data.tpcds_sf100tcl.customer tablesample system(5) repeatable(111);
