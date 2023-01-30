02cc07759c32d72356f71fdfb0dc00b3096765c4
Vishnu Prasad Gettam <60579222+VishnuGettam@users.noreply.github.com>
2023-01-30 21:39:39 +0000

    Update Datasampling.sql

--- Datasampling.sql
+++ Datasampling.sql
@@ -1,4 +1,3 @@
-
 --data sampling with ROW/BERNOLI method or SYSTEM/BLOCK method along with uses FRACTION or FIXED-SIZE ways
 
 --FRACTION METHOD
@@ -25,3 +24,27 @@ select * from snowflake_sample_data.tpcds_sf100tcl.customer sample row(10 rows);
 --we have to assing a number to the SEED(number) which will register to get the same set of data for all executions
 select * from snowflake_sample_data.tpcds_sf100tcl.customer sample row(12) SEED(125);
 select * from snowflake_sample_data.tpcds_sf100tcl.customer tablesample system(5) repeatable(111);
+
+
+--JOIN( Works only for ROW )
+--3 % of data from customer matches with 4 % of data from customer_address .
+select * 
+from snowflake_sample_data.tpcds_sf100tcl.customer cs sample row(3)
+inner join snowflake_sample_data.tpcds_sf100tcl.customer_address ca sample row(4)
+on cs.c_current_addr_sk = ca.ca_address_sk;
+
+--complete customer data with matched 8 % of customer address data .
+select * 
+from snowflake_sample_data.tpcds_sf100tcl.customer cs 
+inner join snowflake_sample_data.tpcds_sf100tcl.customer_address ca sample row(4)
+on cs.c_current_addr_sk = ca.ca_address_sk;
+
+--to do sampling on join records we have to use sub query  
+
+select * from 
+(
+select * 
+from snowflake_sample_data.tpcds_sf100tcl.customer cs 
+inner join snowflake_sample_data.tpcds_sf100tcl.customer_address ca  
+on cs.c_current_addr_sk = ca.ca_address_sk
+) sampled_joined sample row(8);
